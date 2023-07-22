f8a47da609f3fd4e058d91c39d3ed65575c65db0
SultanIndian007 <amantiwari201608@gmail.com>
2023-07-22 05:50:38 +0530

    intialised-proj

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        © 2023 Sultan. All rights reserved
+      </div>
+    </div>
+  )
+}
+
+export default Footer
