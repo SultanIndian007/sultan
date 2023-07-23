af498aa93759610b5118ff7861d1ced1f040d14a
SultanIndian007 <amantiwari201608@gmail.com>
2023-07-23 07:40:31 +0530

    some-changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const Blog = () => {
+const Blog = async () => {
   return (
-    <div className={styles.container}>
-        Blog
+    <div className={styles.mainContainer}>
+        <Link href="/testId" className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      )
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
-
-const Category = () => {
-  return (
-    <div>
-      Category
+import styles from "./page.module.css"
+import Button from '@/components/Button/Button.jsx';
+import Image from 'next/image.js';
+const Category = ({params}) => {
+  console.log(params);
+  return ( 
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+            <Button text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image 
+                className={styles.img}
+                fill={true}
+                src="https://images.pexels.com/photos/14918400/pexels-photo-14918400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  
+                alt=''  
+              />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+            <Button text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image 
+                className={styles.img}
+                fill={true}
+                src="https://images.pexels.com/photos/14918400/pexels-photo-14918400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  
+                alt=''  
+              />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+            <Button text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image 
+                className={styles.img}
+                fill={true}
+                src="https://images.pexels.com/photos/14918400/pexels-photo-14918400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  
+                alt=''  
+              />
+          </div>
+        </div>
     </div>
   )
 }
